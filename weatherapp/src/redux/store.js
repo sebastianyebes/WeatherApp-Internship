14e7fa3e6e65ee91d4ce2cb686a66facaaaa8fa9
Carl Yebes <carlsebastian.yebes@cit.edu>
2023-06-29 15:39:47 +0800

    Remove date-slice

--- weatherapp/src/redux/store.js
+++ weatherapp/src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import weatherReducer from "./features/weather-slice";
 import locationReducer from "./features/location-slice";
-import dateReducer from "./features/date-slice";
 
 export default configureStore({
   reducer: {
     weather: weatherReducer,
     location: locationReducer,
-    date: dateReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
